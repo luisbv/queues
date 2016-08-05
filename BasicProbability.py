7a32247539cf39c53b0f516ec2d5cb251ffcf045
Luis Benavides <luisbv.89@gmail.com>
2016-08-05 13:59:35 -0500

    - En la clase Evento se cambio a diccionario en lugar de set -- Se creo nueva función add para agregar resultados al evento -- Se cambiaron las funciones para intersección y unión de la clase Evento.
    - Se comento el código en la función main explicando las instrucciones linea por linea

--- BasicProbability.py
+++ BasicProbability.py
@@ -1,14 +1,20 @@
+
 class SampleSpace(dict):
     def __init__ (self):
         self.events = list()
 
-    def addEvent(self, e):
-        self.events.append(e)
+    def addEvent(self, event):
+        self.events.append(event)
 
-class Event(set):
-    def __init__ (self, space = None):
+class Event(dict):
+    def __init__ (self, space = None, name = None):
         self.sampleSpace = space
-        set.__init__(self)
+
+    def add(self, result):
+        if result not in self.keys():
+            self[result] = self.sampleSpace[result]
+        else:
+            print "element %d already in event" % result
 
     def empty(self):
         '''check if the set is empty'''
@@ -18,17 +24,31 @@ class Event(set):
         if self.sampleSpace is None:
             return None
         else:
-            return set(self.sampleSpace.keys()) - self
+            return set(self.sampleSpace.keys()) - set(self.keys())
 
     def union(self, event):
         if self.sampleSpace == event.sampleSpace:
-            return self | event
-        return False
+            union = set(self.keys()) | set(event.keys())
+            if len(union) == 0:
+                print "Union is void"
+                return False
+            else:
+                return union
+        else:
+            print "There is no union. Different sample space"
+            return False
 
-    def intersection(self, e):
+    def intersection(self, event):
         if self.sampleSpace == event.sampleSpace:
-            return self & event
-        return False
+            intersection = set(self.keys()) & set(event.keys())
+            if len(intersection) == 0:
+                print "Intersection is void"
+                return False
+            else:
+                return intersection
+        else:
+            print "There is no intersection. Different sample space"
+            return False
 
 def main():
     S = SampleSpace()
@@ -38,21 +58,29 @@ def main():
     S[4] = 0.3
 
     A = Event(S)
-    print A.empty()
-    A.add(1)
-    A.add(2)
-    print A.empty()
-    S.addEvent(A)
+    print "Event A is empty", A.empty() # check if event is empty
+    A.add(1) # add result 1 to event A
+    A.add(2) # add result 2 to event A
+    A.add(1) # trying to add result 1 to event A
+    print "Event A is empty", A.empty() # check if the event is empty one we add the results
+    S.addEvent(A) # add event to the sample space
 
+    # Same process for event A
     B = Event(S)
-    print B.empty()
+    print "Event B is empty", B.empty()
     B.add(3)
     B.add(4)
-    print B.empty()
+    print "Event B is empty", B.empty()
     S.addEvent(B)
-    print S
-    print A.complement()
-    print B.complement()
+    print "Sample Space", S # print sample space
+    print "Intersection (A n B)", A.intersection(B)
+    print "Intersection (B n A)", B.intersection(A)
+    print "Union (A u B)", A.union(B), A.union(B)
+    print "Union (B u A)", B.union(A)
+    print "A complement", A.complement() # print complement of event A
+    print "B complement",B.complement() # print complement of event B
+    print "Event A", A # print event A
+    print "Event B", B # print event B
 
 if __name__ == "__main__":
     main()
