2c092e07aa80ed65ab61ade97a66740530370c50
Luis Benavides <luisbv.89@gmail.com>
2016-08-09 18:25:53 -0500

    Cambiar el nombre de la clase UniformIntegerBetweenAandB(Distribution), por UniformInteger
    Crear clase nueva para generar muestreo pseudo uniforme.
    Se generan primero para N valores de la distribución UniformInteger (con valores entre 0 y 10 por default)
    Los argumentos de entrada son el número de elementos que se quieren obtener para muestrear.

--- Distibutions.py
+++ Distibutions.py
@@ -1,4 +1,4 @@
-from random import random, randint()
+from random import random, randint
 class Distribution():
     def __init__(self):
         print "Distribution"
@@ -17,7 +17,7 @@ class Bernoulli(Distribution):
 class Binomial(Bernoulli):
     def __init__(self, n, p):
         self.rep = n
-        Bernoulli.__init(self, p)
+        Bernoulli.__init__(self, p)
     def random(self):
         return sum([Bernoulli.random(self) for b in xrange(self.rep)])
 
@@ -46,17 +46,61 @@ class NegBin():
     def __init__(self):
         print "not available"
 
-class UniformIntegerBetweenAandB(Distribution):
-    def __init__(self, A=None, B=None):
-        if A is None:
-            self.initial = 0
-        else:
-            self.final = A
-
-        if B is None:
-            self.initial = 0
-        else:
-            self.final = B
+class UniformInteger(Distribution):
+    def __init__(self, A = 0, B = 10):
+        self.initial = A
+        self.final = B
 
     def random(self):
         return randint(self.initial, self.final)
+
+class PseudoUniform(UniformInteger):
+    def __init__(self, N=100):
+        self.N = N
+        self.M = sum([UniformInteger().random() for x in xrange(self.N)])
+        UniformInteger.__init__(self)
+
+    def sample(self, N=None):
+        if N is None:
+            N = self.N
+        k = int(self.M / N) # particles
+        r = self.M % N      # residual particles
+        b = int(r)               # cells with k + 1 particles
+        a = int(N - r)      # cells with k particles
+        values = list() # values
+        if r < N / 2.0:
+            #add
+            add = list()
+            for n in xrange(N):
+                values.append(k)
+
+            for j in xrange(b):
+                while True:
+                    indexRandCell = randint(0, N - 1)
+                    if indexRandCell not in add:
+                        values[indexRandCell] += 1
+                        add.append(indexRandCell)
+                        break
+        else:
+            #remove
+            remove = list()
+            for n in xrange(N):
+                values.append((k+1))
+
+            for j in xrange(a):
+                while True:
+                    indexRandCell = randint(0, N - 1)
+                    if indexRandCell not in remove:
+                        values[indexRandCell] -= 1
+                        remove.append(indexRandCell)
+                        break
+        return values
+
+def main():
+    d0 = UniformInteger()
+    print d0.random()
+    d1 = PseudoUniform(20)
+    print d1.sample()
+
+if __name__ == "__main__":
+    main()
