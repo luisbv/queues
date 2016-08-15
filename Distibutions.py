71bf39a2edcb34c28af48bdebcb2750da07f89bb
Luis Benavides <luisbv.89@gmail.com>
2016-08-15 14:32:35 -0500

    Se termino la clase para la distribución Poisson - La función random regresa una binomial con lambda = n p cuando n -> inc - El valor de n se fija a 10000
    Se agrego la clase para la distribución multinomial
    - Se debe ingresar el numero de intentos y una lista de eventos
    - La función random regresa un diccionario en el que para cada resultado se realiza una binomial con la probabilidad del resultado
    
    Se termina la clase para la distribución geométrica
    - Se agrega la función random en donde regresa el contador de n intenso de una Bernoulli
    
    Se termina clase para Distribución NegBin
    - la función random regresa la suma de n intentos de una distribución geométrica
    
    Se agregaron las impresiones de las nuevas clases creadas

--- Distibutions.py
+++ Distibutions.py
@@ -1,4 +1,5 @@
 from random import random, randint
+import BasicProbability as bs
 class Distribution():
     def __init__(self):
         print "Distribution"
@@ -22,29 +23,45 @@ class Binomial(Bernoulli):
         return sum([Bernoulli.random(self) for b in xrange(self.rep)])
 
 class PseudoPoisson(Binomial):
-    def __init__(self):
-        print "not available"
-    #
-
-class Multinomial():
-    def __init__(self, events):
-        print "not available"
+    def __init__(self, lambda_):
+        n = 10000
+        p = 1.0 * lambda_ / n
+        print lambda_, n, p
+        Binomial.__init__(self, n, p)
 
     def random(self):
-        #return dictionary
-        return
+        return Binomial.random(self)
 
+class Multinomial(Binomial):
+    def __init__(self, n, events):
+        self.multi = dict()
+        for event in events:
+            print event
+            for k in event:
+                p = event[k]
+                Binomial.__init__(self, n, p)
+                self.multi[k] = Binomial.random(self)
 
-class Geometric():
-    #regresar contador hasta que hay un exito en Bernoulli
-    #hacer prueba unitaria para comprobar su prob sin memoria
-    def __init__(self):
-        print "not available"
+    def random(self):
+        return self.multi
 
-class NegBin():
-    #geometrica
-    def __init__(self):
-        print "not available"
+class Geometric(Bernoulli):
+    def __init__(self, p):
+        Bernoulli.__init__(self,p)
+    def random(self):
+        counter = 0
+
+        while Bernoulli.random(self) == 0:
+            counter += 1
+
+        return counter
+
+class NegBin(Geometric):
+    def __init__(self, n, p):
+        self.n = n
+        Geometric.__init__(self, p)
+    def random(self):
+        return sum([Geometric.random(self) for x in xrange(self.n)])
 
 class UniformInteger(Distribution):
     def __init__(self, A = 0, B = 10):
@@ -58,7 +75,7 @@ class PseudoUniform(UniformInteger):
     def __init__(self, N=100):
         self.N = N
         self.M = sum([UniformInteger().random() for x in xrange(self.N)])
-        UniformInteger.__init__(self)
+        #UniformInteger.__init__(self)
 
     def sample(self, N=None):
         if N is None:
@@ -98,9 +115,35 @@ class PseudoUniform(UniformInteger):
 
 def main():
     d0 = UniformInteger()
-    print d0.random()
+    print "UniformInteger",d0.sample(20)
     d1 = PseudoUniform(20)
-    print d1.sample()
+    print "PseudoUniform",d1.sample()
+    d2 = Geometric(0.2)
+    print "Geometric", d2.random()
+    d3 = NegBin(4, 0.2)
+    print "NegBin", d3.random()
+    d4 = PseudoPoisson(10)
+    print "PseudoPoisson", d4.random()
+    d5 = Binomial(10,0.1)
+    print "Binomial", d5.random()
+    #Multinomial
+    S = bs.SampleSpace()
+    S[1] = 0.1
+    S[2] = 0.2
+    S[3] = 0.4
+    S[4] = 0.3
+    A = bs.Event(S)
+    A.add(1) # add result 1 to event A
+    A.add(2) # add result 2 to event A
+    S.addEvent(A) # add event to the sample space
 
+    # Same process for event A
+    B = bs.Event(S)
+    B.add(3)
+    B.add(4)
+    S.addEvent(B) # add event to the sample space
+
+    d6 = Multinomial(10, [A,B])
+    print "Multinomial", d6.random()
 if __name__ == "__main__":
     main()
