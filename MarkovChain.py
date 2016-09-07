c3120cdd90dfd9ea0157ba8684b2091c2c0a1d6e
Luis Benavides <luisbv.89@gmail.com>
2016-09-06 19:19:35 -0500

    Crear una nueva clase para hacer procesos de Markov (Class MarkovProcess) - Atributos iniciales: estado y tiempo que se lleva en cada estado - Se añaden Transiciones pero esta vez con cierta taza (q), se deben de poner el destino (source) y la parte terminal (terminal) junto con la taza de ir de un estado a otro - En la función para un paso similar al de las cadenas de markov, se puede iniciar en uno de los nodos si se indica sino se elige uno al azar. - Para saber a que estado se mueve, se generan tiempo exponenciales de acuerdo a la taza y se elige el mínimo - Se aumenta el tiempo de visita con respecto al tiempo generado de acuerdo a la distribución exponencial.

--- MarkovChain.py
+++ MarkovChain.py
@@ -1,5 +1,5 @@
 from random import random, choice
-
+from Distributions import Exponential
 threshold = 0.000001
 
 class Markov(dict):
@@ -26,7 +26,10 @@ class Markov(dict):
                 self.state = choice(self.keys())
                 self.countVisits[self.state] = 1
                 return self.state
-
+        else:
+            self.state = initial
+            self.countVisits[self.state] = 1
+            return self.state
         try:
             posibleMoves = self[self.state].keys()
         except:
@@ -45,6 +48,47 @@ class Markov(dict):
             self.countVisits[self.state] = 0
         self.countVisits[self.state] += 1
         return self.state
+
+
+class MarkovProcess(dict):
+    def __init__(self):
+        dict.__init__(self)
+        self.state = None
+        self.timeVisits = dict()
+
+    def addTransition(self, source, terminal, q):
+        if source not in self:
+            self[source] = dict()
+
+        if terminal not in self[source]:
+            self[source][terminal] = q
+
+        return
+
+    def step(self, initial=None):
+        if initial is None:
+            if self.state is None:
+                self.state = choice(self.keys())
+                return self.state
+        else:
+            self.state = initial
+            return self.state
+        minimum = 1e10
+        #Select minimum waiting time
+        for terminal in self[self.state]:
+            q = self[self.state][terminal]
+            waitingTime = Exponential(q).random()
+            if waitingTime <= minimum:
+                minimum = waitingTime
+                choiced = terminal
+
+        self.state = choiced
+        if self.state not in self.timeVisits:
+            self.timeVisits[self.state] = 0
+        self.timeVisits[self.state] += minimum
+
+        return self.state
+
 
 def main():
     M = Markov()
@@ -69,5 +113,34 @@ def main():
         M.step()
 
     print M.countVisits
+
+
+    MP = MarkovProcess()
+    ''' Transition Matrix
+        a     b     c     d
+    a  0.00  1.20  3.20  1.80
+    b  1.50  0.00  6.30  8.50
+    c  8.90  6.00  0.00  2.60
+    d  2.80  5.65  4.25  0.00
+    '''
+    MP.addTransition("a", "b", 1.20)
+    MP.addTransition("a", "c", 3.20)
+    MP.addTransition("a", "d", 1.80)
+    MP.addTransition("b", "a", 1.50)
+    MP.addTransition("b", "c", 6.30)
+    MP.addTransition("b", "d", 8.50)
+    MP.addTransition("c", "a", 8.90)
+    MP.addTransition("c", "b", 6.00)
+    MP.addTransition("c", "d", 2.60)
+    MP.addTransition("d", "a", 2.80)
+    MP.addTransition("d", "b", 5.65)
+    MP.addTransition("d", "c", 4.25)
+
+    STEPS = 20000
+    MP.step("d")
+    for s in xrange(STEPS):
+        MP.step()
+
+    print MP.timeVisits
 if __name__ == "__main__":
     main()
