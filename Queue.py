11e525b05cb608e99151e062ffa879843b36fa30
Luis Benavides <luisbv.89@gmail.com>
2016-10-02 19:25:41 -0500

    Cambio en el tiempo del sistema de colas:
    Ahora no avanza el tiempo con un paso determinado, sino solo se actualizada
    cada vez que hay un nuevo evento, llegada (arrival) o  salida (departe).
    
    En la clase Client:
    - Se cambió el nombre de la clase Client a Customer,
    - Se eliminaron las variables sin uso para la nueva implementación.
    - Se guarda el tiempo de llegada al sistema y el de salida (arrivalDepartureTime).
    
    En la clase Server:
    - Se eliminaron variables y funcionessin uso para la nueva implementación.
    - Se modifico la función de seleccionar un servidos.
    
    En la clase Sistem:
    - La función update controla las actualizaciones del sistema
      encuentra el evento que menor tiempo ya que ocurre en primer lugar,
      ya sea llegada o salida.
    - La función newArrival lleva un control de las llegadas (arrivals).
      Se crea un cliente de la clase Customer y se le agrega el tiempo
      de llegada y se asigna a un servidor al azar.
    - La función newDeparture lleva el control de las salidas
      Se le agrega el tiempo de salida al cliente del servidor con menor tiempo
      de evento de salida.
      Se imprimen los valores de entrada y salida del cliente al servidor.
      Se elimina el cliente del inicio de la fila.

--- Queue.py
+++ Queue.py
@@ -1,123 +1,96 @@
 from random import choice
 from Distributions import Exponential
-from MarkovChain import PoissonProcess
 
-LAMBDA = 5
-MIU = 10
+DEBUG = False
+LAMBDA = 10
+MIU = 5
 
-TIME_STEP = 0.1
+TOTAL_TIME = 50
 
-TOTAL_STEPS = 100
-
-TOTAL_WAITING_TIME = 0.0
-
-class Client():
-    def __init__(self, ):
-        self.waitingTime = 0.0
-        self.arrivalTime = 0.0
+class Customer():
+    def __init__(self):
+        self.arrivalDepartureTime = list()
 
 class Server():
-    def __init__(self):
+    def __init__(self, index):
         self.miu = MIU
         self.queue = list()
-        self.maxQueue = 1000
-        self.serviceTime = 0.0
-        self.exits = 0.0
+        self.arrivals = 0
+        self.departures = 0
         self.isBusy = False
-
-    def setServiceTime(self):
-        self.serviceTime = Exponential(self.miu).random()
-
-    def incrementWaitingTimeCustomers(self, t):
-        global TOTAL_WAITING_TIME
-        for c in self.queue:
-            c.waitingTime += t
-            TOTAL_WAITING_TIME += t
+        self.inDeparture = False
+        self.index = index
 
 class Sistem():
     def __init__(self, numberServers):
         self.t = 0.0
-        self.clients = list()
         self.N = 0
-        self.waitingTime = 0.0
-        self.isArrival = False
         self.servers = list()
+        self.departureEvent = list()
+        self.departures = 0
+        self.arrivals = 0
 
         for s in xrange(numberServers):
-            self.servers.append(Server())
+            self.servers.append(Server(s))
 
     def selectServer(self, customer):
-        serverReceiver = choice(self.servers)
-        if len(serverReceiver.queue) < serverReceiver.maxQueue:
-            serverReceiver.queue.append(customer)
-            if not serverReceiver.isBusy:
-                serverReceiver.isBusy = True
+        server = choice(self.servers)
+        server.queue.append(customer)
+        if DEBUG:
+            print "in Server",server.index
+        if not server.isBusy:
+            server.isBusy = True
+            server.arrivals += 1
 
-    def updateTimeServers(self, t):
-        for s in self.servers:
-            if s.isBusy:
-                s.serviceTime -= t
-            if len(s.queue) > 0:
-                s.incrementWaitingTimeCustomers(t)
-
-    def step(self):
+    def update(self):
         if self.t == 0:
-            self.N += 1
-            self.t += Exponential(LAMBDA).random()
-            c = Client()
-            c.arrivalTime = self.t
-            self.selectServer(c)
-            self.isArrival = True
+            self.t = Exponential(LAMBDA).random()
+            self.newArrival()
         else:
-            newArrivalTime = Exponential(LAMBDA).random()
+            arrivalTime = self.t + Exponential(LAMBDA).random()
 
-            newExitTime = 1000
             i = 0
             for s in self.servers:
-                if s.isBusy:
-                    if s.serviceTime == 0:
-                        s.setServiceTime()
-                        if s.serviceTime < newExitTime:
-                            newExitTime = s.serviceTime
-                            serverIndex = i
-                    else:
-                        if s.serviceTime < newExitTime:
-                            newExitTime = s.serviceTime
-                            serverIndex = i
-
+                if s.isBusy and not s.inDeparture:
+                    self.departureEvent.append([self.t + Exponential(s.miu).random(), i])
+                    s.inDeparture = True
                 i += 1
+            self.departureEvent = sorted(self.departureEvent)
 
-            newTimes = [newArrivalTime, newExitTime]
-            minTime = min(newTimes)
-            if newTimes.index(minTime) == 0: # arrival
-                self.N += 1
-                self.updateTimeServers(newArrivalTime)
-                self.t += newArrivalTime
-                c = Client()
-                c.arrivalTime = self.t
-                self.selectServer(c)
-                self.isArrival = True
-            else:# departure
-                self.t += newExitTime
-                self.updateTimeServers(newExitTime)
-                s = self.servers[serverIndex]
-                s.queue.pop(0)
-                s.exits += 1
-                s.serviceTime = 0.0
-                s.isBusy = False
-                self.isArrival = False
+            if len(self.departureEvent) > 0:
+                if arrivalTime < self.departureEvent[0][0]:
+                    self.t = arrivalTime
+                    self.newArrival()
+                else:
+                    departureTime, serverIndex = self.departureEvent.pop(0)
+                    self.t = departureTime
+                    self.newDeparture(serverIndex)
+            else:
+                self.t = arrivalTime
+                self.newArrival()
 
+    def newArrival(self):
+        if DEBUG:
+            print "New Arrival at", t,
+        self.arrivals += 1
+        customer = Customer()
+        customer.arrivalDepartureTime.append(self.t)
+        self.selectServer(customer)
+
+    def newDeparture(self, serverIndex):
+        if DEBUG:
+            print "New Departure from server %d at %f" % (serverIndex, t)
+        self.departures += 1
+        server = self.servers[serverIndex]
+        customer = server.queue.pop(0)
+        customer.arrivalDepartureTime.append(self.t)
+        print "Server", serverIndex, "arrivalTime", customer.arrivalDepartureTime[0],"departureTime",customer.arrivalDepartureTime[1]
+        server.departures += 1
+        server.isBusy = False
+        server.inDeparture = False
 
 S = Sistem(5)
 
-for step in xrange(TOTAL_STEPS + 1):
-    S.step()
-    if step % 10 == 0:
-        i = 0
-        print "Step",step
-        for server in S.servers:
-            print "Server %d with %d clients in queque and %d clients servered" % (i, len(server.queue), server.exits)
-            i += 1
-        print
-
-print "Expected Waiting Time", TOTAL_WAITING_TIME / S.N
+while S.t < TOTAL_TIME:
+    S.update()
+print S.departures, S.arrivals
