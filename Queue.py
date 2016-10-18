d81e9a219a8f061880009d7d11b213be47838022
Luis Benavides <luisbv.89@gmail.com>
2016-10-17 23:07:07 -0500

    Se agrego la clase QueueSystem Es un sistema M/M/S/S/N M Procesos de llegada MArkovianos (gama) M Procesos de Salida Markovianos (mu) S Servidores S Sin colas ( no hay espacio para hacer fila) N Numero de clientes
    La función de simulación controla si hay una llegada o salida al sistema
    Inicialmente solo habrá llegadas creadas con distribución exponencial con parámetro gamma
    
    Si el menor tiempo entre la llegada de un cliente y la salida de un cliente es la llegada:
    - Un cliente intenta ser atendido, si no puede se rechaza y vuelve a calcular su tiempo de espera
    - Se inicia un reloj cuando el sistema esta ocupado y se toma el tiempo en el cual empezó a ocuparse
    
    Si el menor tiempo es una salida
    - Un nuevo cliente tiene su tiempo de llegada al sistema
    - Si el sistema estaba lleno aumenta el tiempo total de ocupación del sistema
    
    El sistema lleva un conteo de intentos realizados y de rechazos

--- Queue.py
+++ Queue.py
@@ -89,8 +89,89 @@ class System():
         server.isBusy = False
         server.inDeparture = False
 
-S = System(5)
 
-while S.t < TOTAL_TIME:
-    S.update()
-print S.departures, S.arrivals
+class QueueSystem():
+    def __init__(self, gamma=0.5, mu=0.5, s=10, n=20):
+        '''
+        System M/M/S/S/N
+        gamma = arrival rate
+        mu = departure rate
+        s = numbers of servers
+        n = numbers of customers
+        '''
+        self.gamma = gamma
+        self.mu = mu
+        self.s = s
+        self.n = n
+        self.t = 0
+
+        # CUSTOMERS
+        # Create N customers (with exponential distribution)
+        self.customers = sorted([Exponential(self.gamma).random() for c in xrange(n)])
+        self.trys = 0
+        self.rejects = 0
+
+        # SERVERS
+        self.busyTime = 0
+        self.startBusy = None
+        self.departures = []
+
+
+    def simulation(self):
+        while self.t < TOTAL_TIME:
+            # if there are no departures
+            # or (there are coustomers and
+            # the next service is a customer arrival)
+            if len(self.departures) == 0 or (len(self.customers) > 0 and self.customers[0] < self.departures[0]):
+                # next time is arrival
+                self.t = self.customers.pop(0)
+                self.trys += 1
+                # if the system is not bussy
+                if len(self.departures) < self.s:
+                    # assign new departure time to the custumer
+                    self.departures.append(self.t + Exponential(self.mu).random())
+                    #sort the departures
+                    self.departures.sort()
+                    # if the server is busy with the new arrival
+                    if len(self.departures) == self.s:
+                        #start the time that the system is busy
+                        self.startBusy = self.t
+                else: # if the system is full
+                    # increment the rejects of customers
+                    self.rejects += 1
+                    # make new time for the customer to be server
+                    self.customers.append(self.t + Exponential(self.gamma).random())
+                    self.customers.sort()
+            else: # if the next services is a departure
+                self.t = self.departures.pop(0)
+                # schedule new arrival
+                self.customers.append(self.t + Exponential(self.gamma).random())
+                self.customers.sort()
+
+                # if the system is full
+                if self.startBusy is not None:
+                    self.busyTime += self.t - self.startBusy
+                    self.startBusy = None
+
+            print self.t
+
+
+
+
+
+
+def main():
+    '''
+    S = System(5)
+
+    while S.t < TOTAL_TIME:
+        S.update()
+
+
+    print S.departures, S.arrivals
+    '''
+    QS = QueueSystem(1.2, 2.0, 5, 10)
+    QS.simulation()
+    print 1.0 * QS.rejects / QS.trys, QS.busyTime / QS.t
+if __name__ == "__main__":
+    main()
